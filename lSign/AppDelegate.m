a4c78617c284fbe7d63442297b5e43df38e07cee
iamcheese-man <Ilyassmekarto9@gmail.com>
2026-04-02 18:27:20 +0100

    Update AppDelegate.m

--- lSign/AppDelegate.m
+++ lSign/AppDelegate.m
@@ -1,13 +1,23 @@
 #import "AppDelegate.h"
 #import "MainViewController.h"
+#import "InstallViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:[[MainViewController alloc] init]];
-    nav.navigationBar.prefersLargeTitles = YES;
-    self.window.rootViewController = nav;
+
+    UINavigationController *signNav = [[UINavigationController alloc] initWithRootViewController:[[MainViewController alloc] init]];
+    signNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Sign" image:[UIImage systemImageNamed:@"signature"] tag:0];
+
+    UINavigationController *installNav = [[UINavigationController alloc] initWithRootViewController:[[InstallViewController alloc] init]];
+    installNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Install" image:[UIImage systemImageNamed:@"arrow.down.app"] tag:1];
+
+    UITabBarController *tabs = [[UITabBarController alloc] init];
+    tabs.viewControllers = @[signNav, installNav];
+    tabs.selectedIndex = 0;
+
+    self.window.rootViewController = tabs;
     [self.window makeKeyAndVisible];
     return YES;
 }
